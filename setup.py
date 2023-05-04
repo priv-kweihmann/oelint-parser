cb290a778756e49e987c5135ce977d409b31109e
priv-kweihmann <priv-kweihmann@localhost>
2023-05-04 04:33:39 +0000

    Release 2.10.5

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('requirements.txt') as f:
 
 setuptools.setup(
     name='oelint_parser',
-    version='2.10.4',
+    version='2.10.5',
     author='Konrad Weihmann',
     author_email='kweihmann@outlook.com',
     description='Alternative parser for bitbake recipes',
