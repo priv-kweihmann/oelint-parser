186af28dc4c0bcbb63bf6acabc435024f526c1b1
Konrad Weihmann <kweihmann@outlook.com>
2023-06-29 11:44:16 +0000

    Deprecate Python 3.7 support
    Closes #90
    
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- setup.py
+++ setup.py
@@ -30,7 +30,6 @@ setuptools.setup(
         'Natural Language :: English',
         'Operating System :: POSIX :: Linux',
         'Programming Language :: Python :: 3 :: Only',
-        'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
