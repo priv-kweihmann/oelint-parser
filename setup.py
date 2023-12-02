b74423ee013d586134ac77e111c75171caccdf0c
Konrad Weihmann <kweihmann@outlook.com>
2023-12-02 07:04:06 +0000

    repo: add python 3.12 support
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- setup.py
+++ setup.py
@@ -34,7 +34,8 @@ setuptools.setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
         'Programming Language :: Python :: 3',
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.8',
 )
