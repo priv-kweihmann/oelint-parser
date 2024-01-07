fe0bf5fe3414db2bf77a72f326304f7edaa6cd70
Konrad Weihmann <kweihmann@outlook.com>
2024-01-07 20:13:09 +0100

    docs: update documentation
    Signed-off-by: Konrad Weihmann <kweihmann@outlook.com>

--- oelint_parser/inlinerep.py
+++ oelint_parser/inlinerep.py
@@ -2,6 +2,14 @@ from oelint_parser.rpl_regex import RegexRpl
 
 
 def bb_utils_contains(_in: str) -> str:
+    """bb.utils.contains emulation
+
+    Args:
+        _in (str): Input string
+
+    Returns:
+        str: True argument of the conditional or None if not applicable
+    """
     m = RegexRpl.match(
         r"(.*)bb\.utils\.contains\(.*?,\s*.*?,\s*(.*?),\s*.*?,\s*.\)", _in)
     if m:
@@ -10,6 +18,14 @@ def bb_utils_contains(_in: str) -> str:
 
 
 def bb_utils_contains_any(_in: str) -> str:
+    """bb.utils.contains_any emulation
+
+    Args:
+        _in (str): Input string
+
+    Returns:
+        str: True argument of the conditional or None if not applicable
+    """
     m = RegexRpl.match(
         r"(.*)bb\.utils\.contains_any\(.*?,\s*.*?,\s*(.*?),\s*.*?,\s*.\)", _in)
     if m:
@@ -18,6 +34,14 @@ def bb_utils_contains_any(_in: str) -> str:
 
 
 def oe_utils_conditional(_in: str) -> str:
+    """oe.utils.conditional emulation
+
+    Args:
+        _in (str): Input string
+
+    Returns:
+        str: True argument of the conditional or None if not applicable
+    """
     m = RegexRpl.match(
         r"(.*)oe\.utils\.conditional\(.*?,\s*.*?,\s*(.*?),\s*.*?,\s*.\)", _in)
     if m:
@@ -26,6 +50,14 @@ def oe_utils_conditional(_in: str) -> str:
 
 
 def oe_utils_ifelse(_in: str) -> str:
+    """oe.utils.ifelse emulation
+
+    Args:
+        _in (str): Input string
+
+    Returns:
+        str: True argument of the conditional or None if not applicable
+    """
     m = RegexRpl.match(r"(.*)oe\.utils\.ifelse\(.*?,\s*(.*?),\s*.*?\)", _in)
     if m:
         return m.group(1) + m.group(2).strip("\"'")
@@ -33,6 +65,14 @@ def oe_utils_ifelse(_in: str) -> str:
 
 
 def inlinerep(_in: str) -> str:
+    """Replaces inline code expressions
+
+    Args:
+        _in (str): Input string
+
+    Returns:
+        str: Expanded string or None, if not applicable
+    """
     _clean_in = _in.lstrip("${@").rstrip("}")
     for x in [bb_utils_contains(_clean_in),
               bb_utils_contains_any(_clean_in),
