48f42a9c181a5b3a46da0ee9a1e3115b5181a89e
Michal Sieron <michalwsieron@gmail.com>
2026-02-22 16:18:40 +0100

    stash/AddFile: bbappends should only match recipes
    Added test_bbappend_doesnt_append_inc case to test that behavior.
    Due to Python's name mangling we have to mangle __map attribute name
    ourselves.
    
    Signed-off-by: Michal Sieron <michalwsieron@gmail.com>

--- tests/test_bbappends.py
+++ tests/test_bbappends.py
@@ -7,6 +7,7 @@ import sys
 class OelintBBAppendsTest(unittest.TestCase):
 
     RECIPE = os.path.join(os.path.dirname(__file__), "testlayer/recipes-appends/test.bb")
+    RECIPE_INC = os.path.join(os.path.dirname(__file__), "testlayer/recipes-appends/test.inc")
     RECIPE_APPEND = os.path.join(os.path.dirname(__file__), "testlayer/recipes-appends/test.bbappend")
 
     def setUp(self):
@@ -36,6 +37,21 @@ class OelintBBAppendsTest(unittest.TestCase):
         self.__stash.Finalize()
         self.assertEqual(len(self.__stash.GetLoneAppends()), 0, msg="No lone appends")
         self.assertEqual(len(self.__stash.GetRecipes()), 1, msg="One recipe")
+
+    def test_bbappend_doesnt_append_inc(self):
+        from oelint_parser.cls_stash import Stash
+        self.__stash = Stash()
+        self.__stash.AddFile(OelintBBAppendsTest.RECIPE_APPEND)
+        self.__stash.Finalize()
+
+        # Python's name mangling forces us to mangle the attribute name ourselves
+        stash_map = self.__stash._Stash__map
+
+        inc_linked_to_bbappend = OelintBBAppendsTest.RECIPE_INC in stash_map.get(OelintBBAppendsTest.RECIPE_APPEND, {})
+        self.assertTrue(inc_linked_to_bbappend, msg="include file is linked to the bbappend")
+
+        bbappend_linked_to_inc = OelintBBAppendsTest.RECIPE_APPEND in stash_map.get(OelintBBAppendsTest.RECIPE_INC, {})
+        self.assertFalse(bbappend_linked_to_inc, msg="bbappend is not linked to the include file")
 
 
 if __name__ == "__main__":
