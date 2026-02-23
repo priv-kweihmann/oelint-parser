d9420a382e328535d16403a39f1a71e51e030270
Michal Sieron <michalwsieron@gmail.com>
2026-02-23 21:50:33 +0100

    test_linenumbers: Check if unrelated recipes impact Line in bbappend
    Make sure that recipes not matching bbappend do not impact Line
    attribute values in items from that bbappend.
    
    Signed-off-by: Michal Sieron <michalwsieron@gmail.com>

--- tests/test_linenumbers.py
+++ tests/test_linenumbers.py
@@ -49,3 +49,59 @@ class TestOelintLineNumbers(TestBaseClass):
         expected = recipe[1] + bbappend[1] if bbappend[1] else 0
         assert max((i.Line for i in bbappend_items), default=0) == expected, \
                 f"Last item in the bbappend is on line {expected}"
+
+    def test_unrelated_recipe(self):
+        from oelint_parser.cls_stash import Stash
+        from oelint_parser.cls_item import Item
+
+        self.__stash = Stash()
+        recipe_path = self.create_tempfile(
+            "myrecipe_3.1.bb",
+            '''
+            A = "21"
+            Z = "foo"
+            ''')
+        self.__stash.AddFile(recipe_path)
+        bbappend_path = self.create_tempfile(
+            "myrecipe_%.bbappend",
+            '''
+            FOO = "abc"
+            BAR = "xyz"
+            BAZ = "123"
+            ''')
+        self.__stash.AddFile(bbappend_path)
+        other_recipe_path = self.create_tempfile(
+            "otherrecipe_2.4.bb",
+            '''
+            M = "42"
+            P = "bar"
+            Q = "alpha"
+            S = "${WORKDIR}/git"
+            ''')
+        self.__stash.AddFile(other_recipe_path)
+        self.__stash.Finalize()
+
+        recipe_items = self.__stash.GetItemsFor(
+            attribute=Item.ATTR_ORIGIN,
+            attributeValue=recipe_path,
+            nolink=True,
+        )
+        expected = 2
+        assert max((i.Line for i in recipe_items), default=0) == expected, \
+                f"Last item in the recipe is on line {expected}"
+
+        bbappend_items = self.__stash.GetItemsFor(
+            attribute=Item.ATTR_ORIGIN,
+            attributeValue=bbappend_path,
+        )
+        expected = 5
+        assert max((i.Line for i in bbappend_items), default=0) == expected, \
+                f"Last item in the bbappend is on line {expected}"
+
+        other_recipe_items = self.__stash.GetItemsFor(
+            attribute=Item.ATTR_ORIGIN,
+            attributeValue=other_recipe_path,
+        )
+        expected = 4
+        assert max((i.Line for i in other_recipe_items), default=0) == expected, \
+                f"Last item in the other recipe is on line {expected}"
